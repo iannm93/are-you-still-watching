84da22813545a6e8eda501eef203d72ce3a00ed1
ianm93 <45110918+iannm93@users.noreply.github.com>
2020-03-29 15:06:53 -0700

    returned movie/show trailer for requested movie/show

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,4 +12,40 @@ $("#search-button").on("click", function (event) {
     console.log(userInput);
     // pass the search function the user input as an argument 
     streamingLocation(userInput);
+    function youtubeAPI(input){
+      $.ajax({
+        method: "GET",
+        url: "https://www.googleapis.com/youtube/v3/search",
+        data:{ 
+          key: "AIzaSyB4F4I4IMaXS_dYV8OgCHKwex9j-rd5s3g",
+          q: input + " trailer",
+          part: "snippet",
+          maxResults: 1,
+          type: "video",
+          videoEmbeddable: true,
+        },
+        // success: function (data){
+        //   embedVideo(data)
+        // },
+        // error : function (response){
+        //   console.log("request failed")
+        // }
+      }).then(function(response){
+        console.log(response)
+        // console.log(response.items[0].id.kind)
+       var trailer="https://www.youtube.com/embed/" + (response.items[0].id.videoId)
+      console.log(trailer)
+        // console.log("https://www.youtube.com/watch?v=" +  trailer )
+        $("#trailers").append($("<iframe>").width(500).height(500).attr("src", trailer))
+      })
+    }
+      // pass the search function the user input as an argument 
+      // streamingLocation(userInput);
+      // movieInfo(userInput);
+      youtubeAPI(userInput)
+      // randomFunction();
+      // $.when(streamingLocation(userInput)).then(function(){
+      //     movieInfo(userInput);
+      //   })
+      // <iframe width="956" height="538" src="https://www.youtube.com/embed/zAGVQLHvwOY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 });
